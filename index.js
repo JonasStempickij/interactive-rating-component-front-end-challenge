21b5fedcb13aa8efbfa27ffaca8fa25a3224543b
JonasStempickij <86933321+JonasStempickij@users.noreply.github.com>
2022-05-12 15:42:27 +0300

    radio buttons and styling

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const ratingResult = document.querySelector('.rating-result');
+const ratingResult = document.querySelector('.rating-result'); // user input showing in thank you card
 
 const ratingSelected = document
-  .querySelectorAll('.rating-btn')
+  .querySelectorAll('input[type=radio]')
   .forEach((rating) => {
     rating.addEventListener('click', () => {
-      const buttonId = rating.id;
-      setActive(buttonId);
-      ratingResult.innerHTML = buttonId;
+      ratingResult.innerHTML = rating.id;
     });
   });
 
-const setActive = (active) => {
-  document.querySelectorAll('.rating-btn').forEach((rating) => {
-    if (rating.id !== active) {
-      rating.className = 'rating-btn';
-    } else rating.className = 'rating-btn active';
-  });
-};
-
+// On submit hide rating card and show thank you card
 const onSubmit = document
   .querySelector('.submit-btn')
   .addEventListener('click', () => {
-    const thankYouCard = document.querySelector('.thank-you-card');
-    thankYouCard.style.display = 'flex';
-    const ratingCard = document.querySelector('.rating-card');
-    ratingCard.style.display = 'none';
+    if (ratingResult.innerHTML) {
+      const thankYouCard = document.querySelector('.thank-you-card');
+      thankYouCard.style.display = 'flex';
+      const ratingCard = document.querySelector('.rating-card');
+      ratingCard.style.display = 'none';
+    } else {
+      const error = document.querySelector('.error');
+      error.style.display = 'block';
+      setTimeout(() => {
+        error.style.display = 'none';
+      }, '5000');
+    }
   });
